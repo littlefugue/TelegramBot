9270dfef090adc7100c28e4606bb4bd0c315aa63
littlefugue <aqweds@naver.com>
2021-06-27 22:27:43 +0900

    my first telegram bot

---
+++ testbot/bot.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+import logging
+
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+load_dotenv()
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+bot = telegram.Bot(TOKEN)
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+
+print(bot.get_me())
+
+def start(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id, text="날 선택해 줄 줄은 정말 몰랐어")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+def echo(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+updater.start_polling()
