c88fec11ea4a26e02a9f8fc464c726b4dcc1e3e1
littlefugue <aqweds@naver.com>
2021-07-02 03:45:24 +0900

    simple changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# TelegramBot
+
+My telegram bots
--- testbot/bot.py
+++ testbot/bot.py
@@ -16,18 +16,20 @@ bot = telegram.Bot(TOKEN)
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
 
-
 print(bot.get_me())
 
+
 def start(update, context):
-	context.bot.send_message(chat_id=update.effective_chat.id, text="날 선택해 줄 줄은 정말 몰랐어")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="날 선택해 줄 줄은 정말 몰랐어")
+
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 
 def echo(update, context):
-	context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
@@ -36,6 +38,7 @@ dispatcher.add_handler(echo_handler)
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
 
 caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
